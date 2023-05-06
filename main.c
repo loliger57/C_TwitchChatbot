92c7c93ba76aae85a210e5159fba270d57e83295
Björn <loliger57@t-online.de>
2023-05-06 16:18:09 +0200

    added zeugs

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include "curl/curl.h"
-int main() {
-    printf("Hello, World!\n");
 
-    extern const char nick[];
+    int main(void)
+    {
+        CURL *curl;
+        CURLcode res;
 
-    return 0;
-}
+        curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, "https://irc.chat.twitch.tv:6667");
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, stdout);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
+
+            const char *oauth = "f2m80b0t3njjsoorpymlpasi1hq492";
+            const char *nick = "lolis__bot";
+            const char *channel = "#loliger57";
+
+            fprintf(stdout, "PASS %s\r\n", oauth);
+            fprintf(stdout, "NICK %s\r\n", nick);
+            fprintf(stdout, "JOIN %s\r\n", channel);
+            fflush(stdout);
+
+            char buffer[4096];
+            while(fgets(buffer, 4096, stdin)) {
+                fprintf(curl, "PRIVMSG %s :%s\r\n", channel, buffer);
+                fflush(stdout);
+            }
+
+            curl_easy_cleanup(curl);
+        }
+
+        return 0;
+    }
+
--- src/data.c
+++ src/data.c
@@ -1,5 +1,5 @@
 
 
-const char oauth[] = "yourname"; //please enter your oauth-code here
-const char nick[] = "yournick"; //please enter the name of the bot accout;
-const char channel[]= "yourchannel"; //enter name of channel to join
+//const char oauth[] = "yourname"; //please enter your oauth-code here
+//const char nick[] = "yournick"; //please enter the name of the bot accout;
+//const char channel[]= "yourchannel"; //enter name of channel to join
